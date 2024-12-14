c2187b65a165b1fb1b775c50bfccca42aacda09e
Tomas <discipleartem@gmail.com>
2024-12-14 18:39:36 +0200

    lesson 5

--- fsite.py
+++ fsite.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-menu = ['Установка', 'Первое приложение', 'Обратная связь']
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
 @app.route('/')
 def index():
-    print(url_for('index'))
+    # print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route('/about',)
 def about():
-    print(url_for('about'))
+    # print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
 # @app.route('/profile/<username>')
