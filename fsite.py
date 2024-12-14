c397f01a6953e4ac6e2a16f1b25e39d636d78fec
Tomas <discipleartem@gmail.com>
2024-12-14 17:54:57 +0200

    compleat lesson 4

--- fsite.py
+++ fsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,14 +7,25 @@ menu = ['Установка', 'Первое приложение', 'Обратн
 
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route('/about',)
 def about():
+    print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
+# @app.route('/profile/<username>')
+# @app.route('/profile/<int:username>')
+# @app.route('/profile/<int:username>/<path>')
+# @app.route('/profile/<float:username>')
+@app.route('/profile/<path:username>') #/profile/OverLord</1234/23423>
+def profile(username):
+    return f'Пользователь: {username}'
 
-
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
 
 
 if __name__ == '__main__':
